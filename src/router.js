5b5d061ab3a5d27ebbc4183d32109731d8b64449
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2023-09-10 11:47:21 +0100

    add accessoryControler and accessories page

--- src/router.js
+++ src/router.js
@@ -2,9 +2,11 @@ const router = require("express").Router()
 
 const homeControler = require("./controllers/homeControler");
 const productControler = require("./controllers/productControler");
+const accessoryControler = require("./controllers/accesssoryControler");
 
 router.use(homeControler);
 router.use("/cubes", productControler);
+router.use("/accessories", accessoryControler);
 router.get("*", (req,res) => {
     res.redirect("/404")
 })

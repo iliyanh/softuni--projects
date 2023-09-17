f7f1942bd8220840eca7f1e15e25c3b38e441add
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2023-09-17 21:19:26 +0100

    add getOthers method

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -2,3 +2,4 @@ const Accessory = require("../models/Accessory");
 
 exports.getAll = () => Accessory.find();
 exports.create = (accessoryData) => Accessory.create(accessoryData);
+exports.getOthers = (accessoryIds) => Accessory.find({_id: {$nin: accessoryIds}})

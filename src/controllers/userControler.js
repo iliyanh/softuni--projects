088192de2eb6f3c18bf2a6302fd8937fb1ec0e81
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2023-09-19 19:40:30 +0100

    add register  plus password validation

--- src/controllers/userControler.js
+++ src/controllers/userControler.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
-
+const userManager = require("../managers/userManager");
 
 router.get("/register", (req,res)=> {
     res.render("users/registerPage")
+})
+router.post("/register", async (req,res)=> {
+    const { username, password, repeatPassword} = req.body;
+    await userManager.register({ username, password, repeatPassword})
+    res.redirect("/users/login")
 })
 router.get("/login", (req,res)=> {
     res.render("users/loginPage")

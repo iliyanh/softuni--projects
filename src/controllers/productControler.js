2f2ec6c1347148d9226cb85c3279bfb20931808d
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2023-09-24 15:46:14 +0100

    update route guards

--- src/controllers/productControler.js
+++ src/controllers/productControler.js
@@ -33,7 +33,7 @@ router.get("/:cubeId/details", async (req, res) => {
    if(!cube){
     return res.redirect("/404")
    }
-   const isOwner = cube.owner?.toString() === req.user._id
+   const isOwner = cube.owner?.toString() === req.user?._id
 
    
     res.render("details", { cube , isOwner})

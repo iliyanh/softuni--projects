31c392363b2fc1724caefbe6b2f0ccea4e5e87ff
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2023-09-24 15:19:35 +0100

    update navbar for user and non user

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -8,6 +8,8 @@ exports.auth = async (req, res, next) => {
         try {
             const user = await jwt.verify(token, SECRET)
             req.user = user;
+            res.locals.user = user;
+            res.locals.isAuthenticated = true;
             next()
         } catch (error) {
             res.clearCookie("auth");
